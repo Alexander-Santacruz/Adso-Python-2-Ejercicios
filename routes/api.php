5aaa6d1f1dc0695d27178d97aa136763d08b4e21
David Alexander Chango Santacruz <davidalexanderchangosantacruz@gmail.com>
2026-09-11 11:38:40 -0500

    Admin sena beta

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/adminsena', function () {
+    return response()->json([
+        'status' => 'success',
+        'message' => 'API de AdminSena funcionando correctamente en Laravel 🚀',
+        'autor' => 'David Alexander Chango Santacruz'
+    ]);
+});
