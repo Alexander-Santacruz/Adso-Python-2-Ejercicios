084453a6cb241f401d1bebe191df80e6c3325ba6
David Alexander Chango Santacruz <davidalexanderchangosantacruz@gmail.com>
2026-09-12 11:36:57 -0500

    fix: verificar y asegurar rutas API correctas en bootstrap/app.php y routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,8 @@ Route::get('/adminsena', function () {
     ]);
 });
 
-Route::get('categories', [CategoryController::class, 'index']);
-Route::post('categories', [CategoryController::class, 'store']);
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::post('/categories', [CategoryController::class, 'store']);
 
-Route::get('courses', [CourseController::class, 'index']);
-Route::post('courses', [CourseController::class, 'store']);
+Route::get('/courses', [CourseController::class, 'index']);
+Route::post('/courses', [CourseController::class, 'store']);
