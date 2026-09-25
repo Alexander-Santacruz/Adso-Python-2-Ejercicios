7c0b0b48007b04c7bc15be68d196d9214a20e69d
David Alexander Chango Santacruz <davidalexanderchangosantacruz@gmail.com>
2026-09-25 10:50:18 -0500

    Añadir rutas DELETE en api.php

--- routes/api.php
+++ routes/api.php
@@ -20,20 +20,25 @@ Route::get('/adminsena', function () {
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::post('/categories', [CategoryController::class, 'store']);
 
-Route::get('/courses', [CourseController::class, 'index']);
-Route::post('/courses', [CourseController::class, 'store']);
-
 Route::get('/areas', [AreaController::class, 'index']);
 Route::post('/areas', [AreaController::class, 'store']);
-
-Route::get('/instructors', [InstructorController::class, 'index']);
-Route::post('/instructors', [InstructorController::class, 'store']);
-
-Route::get('/apprentices', [ApprenticeController::class, 'index']);
-Route::post('/apprentices', [ApprenticeController::class, 'store']);
+Route::delete('/areas/{id}', [AreaController::class, 'destroy']);
 
 Route::get('/training-centers', [TrainingCenterController::class, 'index']);
 Route::post('/training-centers', [TrainingCenterController::class, 'store']);
+Route::delete('/training-centers/{id}', [TrainingCenterController::class, 'destroy']);
+
+Route::get('/courses', [CourseController::class, 'index']);
+Route::post('/courses', [CourseController::class, 'store']);
+Route::delete('/courses/{id}', [CourseController::class, 'destroy']);
+
+Route::get('/apprentices', [ApprenticeController::class, 'index']);
+Route::post('/apprentices', [ApprenticeController::class, 'store']);
+Route::delete('/apprentices/{id}', [ApprenticeController::class, 'destroy']);
+
+Route::get('/instructors', [InstructorController::class, 'index']);
+Route::post('/instructors', [InstructorController::class, 'store']);
+Route::delete('/instructors/{id}', [InstructorController::class, 'destroy']);
 
 Route::get('/computers', [ComputerController::class, 'index']);
 Route::post('/computers', [ComputerController::class, 'store']);
