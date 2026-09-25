1342aef3061e8afad8a582b3c61ac055660b0b7e
David Alexander Chango Santacruz <davidalexanderchangosantacruz@gmail.com>
2026-09-25 10:36:20 -0500

    Quitar prefijo v1 duplicado de AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,13 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->mapApiRoutes();
-    }
-
-    protected function mapApiRoutes(): void
-    {
-        Route::prefix('v1')
-            ->middleware('api')
-            ->group(base_path('routes/api.php'));
+        //
     }
 }
